88219054a7992b7bbce5f7cf78b15e80ec8a7f7e
Your Name <4pa22cs047@pace.edu.in>
2026-04-14 23:46:00 +0530

    Added new files

---
+++ JS/timingfun.js
@@ -0,0 +1,34 @@
+let fun=()=>{
+    console.log('hello')
+}
+
+let num=1
+let fun1=()=>{
+    console.log(num++)
+}
+
+let reset=()=>{
+    num=1
+}
+
+let timer=()=>{
+    let value=1;
+    let val=setInterval(()=>{
+        console.log(value++);
+    },1000)
+    setTimeout(() => {
+        clearInterval(val)
+        console.log("Time UP")
+    }, 10000);
+}
+
+let timer1=()=>{
+    let value=10;
+    let val=setInterval(()=>{
+        console.log(value--);
+    },1000)
+    setTimeout(() => {
+        clearInterval(val)
+        console.log("Time UP")
+    }, 10000);
+}
